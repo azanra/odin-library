2ef06617218f33a28ee35e5dac20a707ed3bbf7b
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-16 17:48:51 +0700

    Remove dummy data

--- script/script.js
+++ script/script.js
@@ -1,27 +1,5 @@
 import { NewBookControlller } from "./controller/newBookController.js";
-import { addBookToLibrary } from "./model/book.js";
-import { bookCard } from "./view/bookCard.js";
 
 let myLibrary = [];
-
-myLibrary = addBookToLibrary(
-  myLibrary,
-  "Three days of happiness",
-  "sugaru miaki",
-  512,
-  "finished"
-);
-
-myLibrary = addBookToLibrary(
-  myLibrary,
-  "Your Story",
-  "sugaru miaki",
-  498,
-  "finished"
-);
-
-console.log(myLibrary);
-
-bookCard(myLibrary);
 
 myLibrary = NewBookControlller(myLibrary);
