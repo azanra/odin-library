739ef4b982631cf19bad1e2ec35c6e07ffb38fb0
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-07 07:57:02 +0700

    Refactor read view to class

--- script/view/read.js
+++ script/view/read.js
@@ -1,35 +1,35 @@
 import { CreateElement } from "./createElement.js";
 import { elementAttr } from "./elementAttr.js";
 
-function Read() {
-  CreateElement(
-    elementAttr.readContainer.parent,
-    elementAttr.readContainer.element,
-    elementAttr.readContainer.attribute
-  );
-  CreateElement(
-    elementAttr.readLabel.parent,
-    elementAttr.readLabel.element,
-    elementAttr.readLabel.attribute,
-    elementAttr.readLabel.text
-  );
-  CreateElement(
-    elementAttr.readSelect.parent,
-    elementAttr.readSelect.element,
-    elementAttr.readSelect.attribute
-  );
-  CreateElement(
-    elementAttr.haveReadOption.parent,
-    elementAttr.haveReadOption.element,
-    elementAttr.haveReadOption.attribute,
-    elementAttr.haveReadOption.text
-  );
-  CreateElement(
-    elementAttr.notReadOption.parent,
-    elementAttr.notReadOption.element,
-    elementAttr.notReadOption.attribute,
-    elementAttr.notReadOption.text
-  );
+export class Read {
+  constructor() {
+    CreateElement(
+      elementAttr.readContainer.parent,
+      elementAttr.readContainer.element,
+      elementAttr.readContainer.attribute
+    );
+    CreateElement(
+      elementAttr.readLabel.parent,
+      elementAttr.readLabel.element,
+      elementAttr.readLabel.attribute,
+      elementAttr.readLabel.text
+    );
+    CreateElement(
+      elementAttr.readSelect.parent,
+      elementAttr.readSelect.element,
+      elementAttr.readSelect.attribute
+    );
+    CreateElement(
+      elementAttr.haveReadOption.parent,
+      elementAttr.haveReadOption.element,
+      elementAttr.haveReadOption.attribute,
+      elementAttr.haveReadOption.text
+    );
+    CreateElement(
+      elementAttr.notReadOption.parent,
+      elementAttr.notReadOption.element,
+      elementAttr.notReadOption.attribute,
+      elementAttr.notReadOption.text
+    );
+  }
 }
-
-export { Read };
