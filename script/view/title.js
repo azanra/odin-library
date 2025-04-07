0957c2d2be13222284b038a05d79df17308d9561
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-07 07:44:45 +0700

    Refactor title view to class

--- script/view/title.js
+++ script/view/title.js
@@ -1,23 +1,23 @@
 import { CreateElement } from "./createElement.js";
 import { elementAttr } from "./elementAttr.js";
 
-function Title() {
-  CreateElement(
-    elementAttr.titleContainer.parent,
-    elementAttr.titleContainer.element,
-    elementAttr.titleContainer.attribute
-  );
-  CreateElement(
-    elementAttr.titleLabel.parent,
-    elementAttr.titleLabel.element,
-    elementAttr.titleLabel.attribute,
-    elementAttr.titleLabel.text
-  );
-  CreateElement(
-    elementAttr.titleInput.parent,
-    elementAttr.titleInput.element,
-    elementAttr.titleInput.attribute
-  );
+export class Title {
+  constructor() {
+    CreateElement(
+      elementAttr.titleContainer.parent,
+      elementAttr.titleContainer.element,
+      elementAttr.titleContainer.attribute
+    );
+    CreateElement(
+      elementAttr.titleLabel.parent,
+      elementAttr.titleLabel.element,
+      elementAttr.titleLabel.attribute,
+      elementAttr.titleLabel.text
+    );
+    CreateElement(
+      elementAttr.titleInput.parent,
+      elementAttr.titleInput.element,
+      elementAttr.titleInput.attribute
+    );
+  }
 }
-
-export { Title };
