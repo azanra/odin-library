df8537fd25d3881dfc6df9e2aa5ea3cbcc8a8dd3
azan-personal <112457939+azanra@users.noreply.github.com>
2025-04-07 08:04:31 +0700

    Refactor form input view to class

--- script/view/form.js
+++ script/view/form.js
@@ -1,39 +1,39 @@
-import { Author } from "./author.js";
+import { AuthorInput } from "./author.js";
 import { CreateElement } from "./createElement.js";
 import { elementAttr } from "./elementAttr.js";
-import { Id } from "./id.js";
-import { Page } from "./page.js";
-import { Read } from "./read.js";
-import { Title } from "./title.js";
+import { IdInput } from "./id.js";
+import { PageInput } from "./page.js";
+import { ReadStatusInput } from "./read.js";
+import { TitleInput } from "./title.js";
 
-function Form() {
-  CreateElement(
-    elementAttr.modal.parent,
-    elementAttr.modal.element,
-    elementAttr.modal.attribute
-  );
-  CreateElement(
-    elementAttr.form.parent,
-    elementAttr.form.element,
-    elementAttr.form.attribute
-  );
-  const idInput = new Id();
-  Title();
-  Author();
-  Page();
-  Read();
-  CreateElement(
-    elementAttr.submitBtn.parent,
-    elementAttr.submitBtn.element,
-    elementAttr.submitBtn.attribute,
-    elementAttr.submitBtn.text
-  );
-  CreateElement(
-    elementAttr.cancelBtn.parent,
-    elementAttr.cancelBtn.element,
-    elementAttr.cancelBtn.attribute,
-    elementAttr.cancelBtn.text
-  );
+export class FormInput {
+  constructor() {
+    CreateElement(
+      elementAttr.modal.parent,
+      elementAttr.modal.element,
+      elementAttr.modal.attribute
+    );
+    CreateElement(
+      elementAttr.form.parent,
+      elementAttr.form.element,
+      elementAttr.form.attribute
+    );
+    const idInput = new IdInput();
+    const titleInput = new TitleInput();
+    const authorInput = new AuthorInput();
+    const pageInput = new PageInput();
+    const readStatusInput = new ReadStatusInput();
+    CreateElement(
+      elementAttr.submitBtn.parent,
+      elementAttr.submitBtn.element,
+      elementAttr.submitBtn.attribute,
+      elementAttr.submitBtn.text
+    );
+    CreateElement(
+      elementAttr.cancelBtn.parent,
+      elementAttr.cancelBtn.element,
+      elementAttr.cancelBtn.attribute,
+      elementAttr.cancelBtn.text
+    );
+  }
 }
-
-export { Form };
